14bb2a6bc839ccc623aacdd1aa40021d9126bc08
Silas Berger <SilasBerger@users.noreply.github.com>
2020-11-03 11:26:37 +0100

    Finalize setup (#3)
    * Switch favicon.
    
    * Add Zuehlke logos.
    
    * Clean up styling setup.
    
    * Implement ZueBanner component as a reference.
    
    * Clean up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Hero from "./components/Hero/Hero";
+import ZueBanner from "./components/ZueBanner/ZueBanner";
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <ZueBanner/>
     </div>
   );
 }
