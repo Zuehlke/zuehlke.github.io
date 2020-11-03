2c71f0d2e8b21e0cdda47443783c112938eeda60
Silas Berger <silas.berger@zuehlke.com>
2020-11-03 17:14:48 +0100

    Remove non-existing util classes in hero.

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className="Hero">
-      <article className="responsive-container f-zue-regular color-off-black inner">
+      <article className="inner">
         Producing cool, innovative solutions and solving tough technical problems is what Zühlke lives for and has been
         successful at over several decades. Knowledge sharing is something we cultivate and we strongly believe in the
         power of the Open Source Software communities.
