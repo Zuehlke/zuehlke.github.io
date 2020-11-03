5c56f64d734b5c60f2cb4b3dbbc9f91229f27489
Schoenenberger, Christoph <christoph.schoenenberger@zuehlke.com>
2020-11-03 17:07:08 +0100

    Added stay in touch part including icons

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import Hero from "./components/Hero/Hero";
 import ZueBanner from "./components/ZueBanner/ZueBanner";
 import Footer from "./components/Footer/Footer";
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab)
+
 function App() {
   return (
     <div className="App">
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import './Footer.scss';
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import logo from '../../assets/images/logo/logo-zuehlke-big.png'
 
 const Footer = () => {
@@ -19,6 +20,25 @@ const Footer = () => {
             <li><a href="https://www.zuehlke.com/en/legal-notice" target="_blank">Legal</a></li>
             <li><a href="https://www.zuehlke.com/en/privacy-policy" target="_blank">Terms of Use & Data Privacy</a></li>
           </ul>
+        </div>
+        <div className="stay-in-touch">
+            <div className="row">
+                Stay in touch
+            </div>
+            <div className="row">
+                <div className="col">
+                    <a href="https://www.linkedin.com/company/zuehlkegroup/" target="_blank"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+                    <a href="https://github.com/Zuehlke" target="_blank"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+                </div>
+                <div className="col">
+                    <a href="https://www.facebook.com/zuehlke.group" target="_blank"><FontAwesomeIcon icon={['fab', 'facebook-square']} /></a>
+                    <a href="https://www.instagram.com/zuehlkegroup/?hl=en" target="_blank"><FontAwesomeIcon icon={['fab', 'instagram']} /></a>
+                </div>
+                <div className="col">
+                    <a href="https://twitter.com/zuehlke_group" target="_blank"><FontAwesomeIcon icon={['fab', 'twitter-square']} /></a>
+                    <a href="https://www.youtube.com/channel/UCDglr0_rdf7cIakhluxAeBA" target="_blank"><FontAwesomeIcon icon={['fab', 'youtube']} /></a>
+                </div>
+            </div>
         </div>
       </div>
     </div>
