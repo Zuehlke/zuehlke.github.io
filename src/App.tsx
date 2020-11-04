2315e1aa989e78d377af54551e2e09ffbf5fc694
Silas Berger <SilasBerger@users.noreply.github.com>
2020-11-04 10:35:39 +0100

    Implement footer (#5)
    * Added logo and links
    
    * Added stay in touch part including icons
    
    * Incorporating _spacing.scss in footer stuff and clean-up
    
    * Updating package.json and package-lock.json
    
    Co-authored-by: Schoenenberger, Christoph <christoph.schoenenberger@zuehlke.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.scss';
 import Hero from "./components/Hero/Hero";
 import ZueBanner from "./components/ZueBanner/ZueBanner";
+import Footer from "./components/Footer/Footer";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+library.add(fab)
 
 function App() {
   return (
     <div className="App">
       <Hero/>
       <ZueBanner/>
+      <Footer/>
     </div>
   );
 }
