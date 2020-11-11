7709a3a26ef3acbf591cd611db7b00e2bca39a88
Silas Berger <silas.berger@zuehlke.com>
2020-11-11 17:24:44 +0100

    Work on person tile.

--- src/components/PersonTile/PersonTile.tsx
+++ src/components/PersonTile/PersonTile.tsx
@@ -9,14 +9,16 @@ type Props = {
 const PersonTile = (props: Props) => {
   return (
     <div className="PersonTile">
-      <div className="profile-container">
-        <img src={props.person.logoUrl} className="avatar" alt={`Avatar ${props.person.title}`}/>
-        <span className="title">{props.person.title}</span>
-        <span>{props.person.name}</span>
-      </div>
-      <div className="description-container">
-        <span className="description">{props.person.description}</span>
-      </div>
+      <a href={props.person.url} target="_blank" rel="noreferrer">
+        <div className="profile-container">
+          <img src={props.person.logoUrl} className="avatar" alt={`Avatar ${props.person.title}`}/>
+          <span className="title text">{props.person.title}</span>
+          <span className="name text">{props.person.name}</span>
+        </div>
+        <div className="description-container">
+          <span className="description">{props.person.description}</span>
+        </div>
+      </a>
     </div>
   );
 };
