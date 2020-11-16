8eb64f35c34b6c0d696c2867b2193f5e941656c6
Silas Berger <silas.berger@zuehlke.com>
2020-11-16 16:29:02 +0100

    Finish response parser and tests.

--- github/github_response_parser.py
+++ github/github_response_parser.py
@@ -1,16 +1,15 @@
 def parse_response_item(schema, response_item):
-    if schema == {}:
+    if (schema == {}) or (schema == []):
         # Base case - this response item does not need to be further destructured.
         return response_item
+    if type(schema) is list:
+        # Schema expects this node to be a list - process children individually and collect results.
+        assert len(schema) == 1, "List schema node can only contain 0 or 1 elements."
+        assert type(response_item) is list, "Schema expected list, but response item was not list."
+        schema = schema[0]
+        return [parse_response_item(schema, item) for item in response_item]
+    # Process this item as a dictionary.
     result = {}
     for key, schema_value in schema.items():
         result[key] = parse_response_item(schema_value, response_item[key])
-    return result
-
-
-def parse_response_item_array(schema, response_array):
-    result = {}
-    for response_item in response_array:
-        parsed_item = parse_response_item(schema, response_item)
-        result[parsed_item["id"]] = parsed_item
     return result
--- github/test/test_github_response_parser.py
+++ github/test/test_github_response_parser.py
@@ -266,3 +266,315 @@ class TestGithubResponseParser(unittest.TestCase):
         }
         actual = github_response_parser.parse_response_item(schema, response_item)
         self.assertEqual(expected, actual)
+
+    def test__parse_response_item__root_list__no_schema__should_select_all(self):
+        schema = []
+        response_item = [
+            {"name": "Jon Doe"},
+            {"name": "Jane Doe"}
+        ]
+        expected = [
+            {"name": "Jon Doe"},
+            {"name": "Jane Doe"}
+        ]
+        actual = github_response_parser.parse_response_item(schema, response_item)
+        self.assertEqual(expected, actual)
+
+    def test__parse_response_item__root_list__should_explicitly_select_all(self):
+        schema = [{
+            "name": {}
+        }]
+        response_item = [
+            {"name": "Jon Doe"},
+            {"name": "Jane Doe"}
+        ]
+        expected = [
+            {"name": "Jon Doe"},
+            {"name": "Jane Doe"}
+        ]
+        actual = github_response_parser.parse_response_item(schema, response_item)
+        self.assertEqual(expected, actual)
+
+    def test__parse_response_item__root_list__nested_elements__should_ignore_age_lastname(self):
+        schema = [{
+            "name": {
+                "first": {}
+            }
+        }]
+        response_item = [
+            {
+                "name": {
+                    "first": "Jon",
+                    "last": "Doe"
+                },
+                "age": 42
+            },
+            {
+                "name": {
+                    "first": "Jane",
+                    "last": "Doe"
+                },
+                "age": 85
+            }
+        ]
+        expected = [
+            {
+                "name": {
+                    "first": "Jon"
+                },
+            },
+            {
+                "name": {
+                    "first": "Jane"
+                }
+            }
+        ]
+        actual = github_response_parser.parse_response_item(schema, response_item)
+        self.assertEqual(expected, actual)
+
+    def test__parse_response_item__child_list__nested_elements__should_ignore_list(self):
+        schema = {
+            "id": {},
+            "title": {}
+        }
+        response_item = {
+            "id": "1234",
+            "title": "hello-world",
+            "contributors": [
+                {
+                    "id": "1111",
+                    "login": "jondoe"
+                },
+                {
+                    "id": "2222",
+                    "login": "janedoe"
+                }
+            ]
+        }
+        expected = {
+            "id": "1234",
+            "title": "hello-world"
+        }
+        actual = github_response_parser.parse_response_item(schema, response_item)
+        self.assertEqual(expected, actual)
+
+    def test__parse_response_item__child_list__nested_elements__should_implicitly_select_full_items(self):
+        schema = {
+            "id": {},
+            "title": {},
+            "contributors": []
+        }
+        response_item = {
+            "id": "1234",
+            "title": "hello-world",
+            "contributors": [
+                {
+                    "id": "1111",
+                    "login": "jondoe"
+                },
+                {
+                    "id": "2222",
+                    "login": "janedoe"
+                }
+            ]
+        }
+        expected = {
+            "id": "1234",
+            "title": "hello-world",
+            "contributors": [
+                {
+                    "id": "1111",
+                    "login": "jondoe"
+                },
+                {
+                    "id": "2222",
+                    "login": "janedoe"
+                }
+            ]
+        }
+        actual = github_response_parser.parse_response_item(schema, response_item)
+        self.assertEqual(expected, actual)
+
+    def test__parse_response_item__child_list__nested_elements__should_explicitly_select_full_items(self):
+        schema = {
+            "id": {},
+            "title": {},
+            "contributors": [{
+                "id": {},
+                "login": {}
+            }]
+        }
+        response_item = {
+            "id": "1234",
+            "title": "hello-world",
+            "contributors": [
+                {
+                    "id": "1111",
+                    "login": "jondoe"
+                },
+                {
+                    "id": "2222",
+                    "login": "janedoe"
+                }
+            ]
+        }
+        expected = {
+            "id": "1234",
+            "title": "hello-world",
+            "contributors": [
+                {
+                    "id": "1111",
+                    "login": "jondoe"
+                },
+                {
+                    "id": "2222",
+                    "login": "janedoe"
+                }
+            ]
+        }
+        actual = github_response_parser.parse_response_item(schema, response_item)
+        self.assertEqual(expected, actual)
+
+    def test__parse_response_item__child_list__nested_elements__should_ignore_title_login(self):
+        schema = {
+            "id": {},
+            "contributors": [{
+                "id": {}
+            }]
+        }
+        response_item = {
+            "id": "1234",
+            "title": "hello-world",
+            "contributors": [
+                {
+                    "id": "1111",
+                    "login": "jondoe"
+                },
+                {
+                    "id": "2222",
+                    "login": "janedoe"
+                }
+            ]
+        }
+        expected = {
+            "id": "1234",
+            "contributors": [
+                {
+                    "id": "1111",
+                },
+                {
+                    "id": "2222",
+                }
+            ]
+        }
+        actual = github_response_parser.parse_response_item(schema, response_item)
+        self.assertEqual(expected, actual)
+
+    def test__parse_response_item__complex_nested_objects_and_lists(self):
+        schema = [{
+            "id": {},
+            "title": {},
+            "contributors": [
+                {
+                    "id": {},
+                    "repos": [
+                        {
+                            "id": {}
+                        }
+                    ]
+                }
+            ]
+        }]
+        response_item = [
+            {
+                "id": "1234",
+                "title": "hello-world",
+                "contributors": [
+                    {
+                        "id": "1111",
+                        "login": "jondoe",
+                        "repos": [
+                            {
+                                "id": "1234",
+                                "title": "hello-world"
+                            },
+                            {
+                                "id": "5678",
+                                "title": "hello-earth"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "2222",
+                        "login": "janedoe",
+                        "repos": [
+                            {
+                                "id": "1234",
+                                "title": "hello-world"
+                            }
+                        ]
+                    },
+                ],
+                "stargazers_count": 42
+            },
+            {
+                "id": "9876",
+                "title": "project1",
+                "contributors": [
+                    {
+                        "id": "3333",
+                        "login": "tommytest",
+                        "repos": [
+                            {
+                                "id": "9876",
+                                "title": "project1"
+                            }
+                        ]
+                    },
+                ],
+                "stargazers_count": 2344
+            }
+        ]
+        expected = [
+            {
+                "id": "1234",
+                "title": "hello-world",
+                "contributors": [
+                    {
+                        "id": "1111",
+                        "repos": [
+                            {
+                                "id": "1234"
+                            },
+                            {
+                                "id": "5678"
+                            }
+                        ]
+                    },
+                    {
+                        "id": "2222",
+                        "repos": [
+                            {
+                                "id": "1234"
+                            }
+                        ]
+                    },
+                ]
+            },
+            {
+                "id": "9876",
+                "title": "project1",
+                "contributors": [
+                    {
+                        "id": "3333",
+                        "repos": [
+                            {
+                                "id": "9876"
+                            }
+                        ]
+                    },
+                ]
+            }
+        ]
+        actual = github_response_parser.parse_response_item(schema, response_item)
+        self.assertEqual(expected, actual)
